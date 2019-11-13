f34acc3c95295c45d3f8e0422c02e74fa8f4bf31
OxiBo <xenos2000@ukr.net>
2019-11-12 22:44:51 -0500

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = (env) => {
-    // console.log("env:", env)
-    const isProduction = env === "production";
-    const CSSExtract = new ExtractTextPlugin('styles.css')
+module.exports = env => {
+  // console.log("env:", env)
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
   return {
     entry: "./src/app.js",
@@ -20,35 +20,30 @@ module.exports = (env) => {
           exclude: /node_modules/
         },
         {
-        
-                // test: /.s?css$/, // question mark makes it test for .scss and .css files
-                // use: ["style-loader", "css-loader", "sass-loader"]
-            
-
+          // test: /.s?css$/, // question mark makes it test for .scss and .css files
+          // use: ["style-loader", "css-loader", "sass-loader"]
 
           test: /.s?css$/, // question mark makes it test for .scss and .css files
           use: CSSExtract.extract({
-              use: [
-                  {
-                      loader: "css-loader",
-                      options: {
-                          sourceMap: true
-                      }
-                  },//was 'css-loader',
-                 {
-                     loader: 'sass-loader',
-                     options: {
-                         sourceMap: true
-                     }
-                 } // was 'sass-loader'
-              ]
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              }, //was 'css-loader',
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              } // was 'sass-loader'
+            ]
           })
         }
       ]
     },
-    plugins: [
-        CSSExtract
-    ],
+    plugins: [CSSExtract],
     devtool: isProduction ? "source-map" : "inline-source-map", // devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
@@ -58,7 +53,6 @@ module.exports = (env) => {
 };
 
 // to get new class syntax install https://www.npmjs.com/package/babel-plugin-transform-class-properties
-
 
 // {
 //     test: /.s?css$/, // question mark makes it test for .scss and .css files
